04c384518f6204393726968d08d8422b3956cd7d
Jeffrey Zhao <jeffz@live.com>
2012-06-02 12:55:16 +0800

    automatic index generation

--- scripts/manuals.js
+++ scripts/manuals.js
@@ -1,3 +1,5 @@
+"use strict";
+
 document.createElement("header");
 document.createElement("nav");
 document.createElement("aside");
@@ -5,12 +7,51 @@ document.createElement("footer");
 document.createElement("section");
 
 $(document).ready(function () {
-    var tocList = $("#toc-list").empty();
+
+    var currentList = $("#toc-list").empty();
+    var currentLevel = 2;
     
-    var i = 0;
-    $("#container > h2").each(function () {
-        var id = "h_" + (i++);
-        var link = $("<a>").attr("href", "#" + id).html($(this).attr("id", id).html());
-        $("<li>").append(link).appendTo(tocList);
-    });
+    var getName = function (h) {
+        var prevP = $(h).prev()[0];
+        if (!prevP) return;
+        if (prevP.tagName.toLowerCase() != "p") return;
+        
+        var link = prevP.firstChild;
+        if (!link) return;
+        if (!link.tagName) return;
+        if (link.tagName.toLowerCase() != "a") return;
+        if (link.innerHTML != "") return;
+        
+        return link.name;
+    }
+    
+    var getLevel = function (h) {
+        return h.tagName.toLowerCase() == "h2" ? 2 : 3;
+    }
+    
+    var getLink = function (h, name) {
+        return $("<a>").attr("href", "#" + name).html($(h).html());
+    }
+    
+    var addToList = function () {
+        var name = getName(this);
+        if (!name) return;
+        
+        var level = getLevel(this);
+        if (level > currentLevel) {
+            var children = currentList.children();
+            var lastItem = children[children.length - 1];
+            
+            var newList = $("<ul>").appendTo(lastItem);
+            currentList = newList;
+        } else if (level < currentLevel) {
+            currentList = currentList.parent().parent();
+        }
+        
+        currentLevel = level;
+        
+        $("<li>").append(getLink(this, name)).appendTo(currentList);
+    }
+    
+    $("#container > h2, #container > h3").each(addToList);
 });
