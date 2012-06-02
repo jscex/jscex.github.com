677ca26b50eda8140dc840a72f8ba4d8b926115b
Jeffrey Zhao <jeffz@live.com>
2012-06-02 09:53:19 +0800

    test manuals

---
+++ scripts/site.js
@@ -0,0 +1,16 @@
+document.createElement("header");
+document.createElement("nav");
+document.createElement("aside");
+document.createElement("footer");
+document.createElement("section");
+
+$(document).ready(function () {
+    var tocList = $("#toc-list").empty();
+    
+    var i = 0;
+    $("#main h2").each(function () {
+        var id = "h_" + (i++);
+        var link = $("<a>").attr("href", "#" + id).html($(this).attr("id", id).html());
+        $("<li>").append(link).appendTo(tocList);
+    });
+});
